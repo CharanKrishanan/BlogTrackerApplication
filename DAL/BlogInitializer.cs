6ae627996bbf1aad23b34fc792b09d495c5fbbf3
CharanKrishnan <charankovvali@gmail.com>
2023-12-18 22:16:03 +0530

    Add project files.

---
+++ DAL/BlogInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class BlogInitializer : DropCreateDatabaseIfModelChanges<BlogContext>
+    {
+        protected override void Seed(BlogContext context)
+        {
+            List<AdminInfo> adminInfos = new List<AdminInfo>();
+
+            adminInfos.Add(new AdminInfo() { Email = "charankovvali@gmail.com", Password = "charan@123" });
+            adminInfos.Add(new AdminInfo() { Email = "sai@gmail.com", Password = "sai@123" });
+
+            context.AdminInfos.AddRange(adminInfos);
+            context.SaveChanges();
+
+            List<EmpInfo> empInfos = new List<EmpInfo>();
+            empInfos.Add(new EmpInfo() { Email = "sahithi@gmail.com", PassCode = 7, DateOfJoining = new DateTime(2019, 01, 05), Name = "Sahithi" });
+            empInfos.Add(new EmpInfo() { Email = "sivasai@gmail.com", PassCode = 8, DateOfJoining = new DateTime(2020, 06, 08), Name = "Sivasai" });
+            empInfos.Add(new EmpInfo() { Email = "vara@gmail.com", PassCode = 9, DateOfJoining = new DateTime(2020, 10, 05), Name = "vara" });
+            context.EmpInfos.AddRange(empInfos);
+            context.SaveChanges();
+
+            List<BlogInfo> blogInfos = new List<BlogInfo>();
+            blogInfos.Add(new BlogInfo() { Email = "vara@gmail.com", BlogUrl = "https://mercedesblog.com/", DateOfCreation = new DateTime(2021, 06, 10), Subject = "Engine", Title = "Mercedes" });
+            blogInfos.Add(new BlogInfo() { Email = "sivasai@gmail.com", BlogUrl = "https://www.digitalcameraworld.com/reviews/canon-rf-200-800mm-f63-9-is-usm-review", DateOfCreation = new DateTime(2020, 04, 11), Subject = "Lens", Title = "Canon" });
+            blogInfos.Add(new BlogInfo() { Email = "sahithi@gmail.com", BlogUrl = "https://www.jigsawplanet.com/", DateOfCreation = new DateTime(2021, 02, 04), Subject = "Solving", Title = "Puzzles" });
+            context.BlogInfos.AddRange(blogInfos);
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ Testing/Moq.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Moq;
+
+namespace Testing
+{
+    [TestFixture]
+    public class Moq
+    {
+        [Test]
+        public void EmpInfo_ValidProperties_Success()
+        {
+            // Arrange
+            var empInfo = new EmpInfo
+            {
+                Email = "test@example.com",
+                Name = "John Doe",
+                DateOfJoining = DateTime.Now,
+                PassCode = 1234
+            };
+
+            // Act
+            // No action required for this test
+
+            // Assert
+            Assert.That(empInfo.Email, Is.EqualTo("test@example.com"));
+            Assert.That(empInfo.Name, Is.EqualTo("John Doe"));
+            Assert.That(empInfo.DateOfJoining, Is.EqualTo(DateTime.Now).Within(TimeSpan.FromSeconds(1)));
+            Assert.That(empInfo.PassCode, Is.EqualTo(1234));
+        }
+
+        
+
+        [Test]
+        public void EmpInfo_Validation_EmailIsRequired()
+        {
+            // Arrange
+            var empInfo = new EmpInfo();
+
+            // Act
+            var validationContext = new ValidationContext(empInfo, null, null);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(empInfo, validationContext, validationResults, true);
+
+            // Assert
+            Assert.That(validationResults, Has.Count.EqualTo(2));
+            Assert.That(validationResults[0].MemberNames, Contains.Item("Email"));
+           
+        }
+
+        // Add similar tests for other validation attributes as needed
+
+        [Test]
+        public void EmpInfo_BlogsCollection_Mocked_Success()
+        {
+            // Arrange
+            var empInfo = new EmpInfo();
+            var mockBlogInfo = new Mock<BlogInfo>();
+
+            // Act
+            empInfo.Blogs = new List<BlogInfo> { mockBlogInfo.Object };
+
+            // Assert
+            Assert.That(empInfo.Blogs, Is.Not.Null);
+            Assert.That(empInfo.Blogs, Has.Count.EqualTo(1));
+            Assert.That(empInfo.Blogs, Contains.Item(mockBlogInfo.Object));
+        }
+    }
+}
